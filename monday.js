58d2b80a5d51edcc54839926bfd67a420b4018df
Kay <mccray382@gmail.com>
2024-10-28 10:11:11 -0400

    first commit

---
+++ monday.js
@@ -0,0 +1,9 @@
+const fruit = ["apple","banana","cherry"]
+
+fruit.forEach(f => console.log(f));
+
+[999,444,222].forEach(n=>console.log(n));
+
+["apple","banana","cherry"].forEach((f,i,a) => console.log(i,f,a));
+
+[11,14,16,25].forEach((n,i,a)=> i ? console.log(n - a[i-1]) : 0);
